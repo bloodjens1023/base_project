773a69b22307d1c15ccae30b526bea1dcc19b72c
unknown <arsenefaly1@gmail.com>
2026-02-17 23:06:02 +0300

    Modif SectionBottom IMGi

--- app/components/Home/BottomSection.vue
+++ app/components/Home/BottomSection.vue
@@ -188,47 +188,23 @@
     <!-- Les 9 Icons  -->
     <div class="px-8 pt-8 mt-8">
       <div class="flex justify-center items-center gap-2 mb-2">
-        <div class="share-icon" >
+        <div class="bg-primary rounded-xl">
           <img
-            src="https://downpg117.uqianbao.com/source/public/static/svg/GZ2uwZ4U.svg"
+            src="/asset/svg/GZ2uwZ4U.svg"
             alt=""
             class="w-full rounded-lg"
           />
         </div>
-        <div class="share-icon" >
+        <div class="bg-primary rounded-xl" >
           <img
-            src="https://downpg117.uqianbao.com/source/public/static/svg/Civ14LDW.svg"
+            src="/asset/svg/Civ14LDW.svg"
             alt=""
             class="w-full rounded-lg"
           />
         </div>
-        <div class="share-icon" >
+        <div class="bg-primary rounded-xl" >
           <img
-            src="https://downpg117.uqianbao.com/source/public/static/svg/B6TXnIDj.svg"
-            alt=""
-            class="w-full rounded-lg"
-          />
-        </div>
-      </div>
-
-      <div class="flex justify-center items-center gap-2 mb-2">
-        <div class="share-icon" >
-          <img
-            src="https://downpg117.uqianbao.com/source/public/static/svg/rP1oFiSh.svg"
-            alt=""
-            class="w-full rounded-lg"
-          />
-        </div>
-        <div class="share-icon" >
-          <img
-            src="https://downpg117.uqianbao.com/source/public/static/svg/D_i-KO_s.svg"
-            alt=""
-            class="w-full rounded-lg"
-          />
-        </div>
-        <div class="share-icon" >
-          <img
-            src="https://downpg117.uqianbao.com/source/public/static/svg/CDma3emR.svg"
+            src="/asset/svg/B6TXnIDj.svg"
             alt=""
             class="w-full rounded-lg"
           />
@@ -236,23 +212,47 @@
       </div>
 
       <div class="flex justify-center items-center gap-2 mb-2">
-        <div class="share-icon" >
+        <div class="bg-primary rounded-xl" >
           <img
-            src="https://downpg117.uqianbao.com/source/public/static/svg/B5U0gd8x.svg"
+            src="/asset/svg/rP1oFiSh.svg"
             alt=""
             class="w-full rounded-lg"
           />
         </div>
-        <div class="share-icon" >
+        <div class="bg-primary rounded-xl" >
           <img
-            src="https://downpg117.uqianbao.com/source/public/static/svg/BVy-OIx7.svg"
+            src="/asset/svg/D_i-KO_s.svg"
             alt=""
             class="w-full rounded-lg"
           />
         </div>
-        <div class="share-icon" >
+        <div class="bg-primary rounded-xl" >
           <img
-            src="https://downpg117.uqianbao.com/source/public/static/svg/CllcxtMJ.svg"
+            src="/asset/svg/CDma3emR.svg"
+            alt=""
+            class="w-full rounded-lg"
+          />
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center gap-2 mb-2">
+        <div class="bg-primary rounded-xl" >
+          <img
+            src="/asset/svg/B5U0gd8x.svg"
+            alt=""
+            class="w-full rounded-lg"
+          />
+        </div>
+        <div class="bg-primary rounded-xl" >
+          <img
+            src="/asset/svg/BVy-OIx7.svg"
+            alt=""
+            class="w-full rounded-lg"
+          />
+        </div>
+        <div class="bg-primary rounded-xl" >
+          <img
+            src="/asset/svg/CllcxtMJ.svg"
             alt=""
             class="w-full rounded-lg"
           />
